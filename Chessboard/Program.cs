414df9fe89a15b1ba7beb634fabe09433a2529fd
Linus Karjalainen <linus.karja@gmail.com>
2026-09-09 10:30:57 +0200

    small improvements

--- Chessboard/Program.cs
+++ Chessboard/Program.cs
@@ -3,6 +3,7 @@
     private static void Main(string[] args)
     {
         //1. Användaren anger input, felhanteras of TryParse() i if-sats
+
         Console.Write("Hur stor ska brädet vara? skriv mellan 3 och 50: ");
 
         string size = Console.ReadLine();
@@ -10,6 +11,7 @@
         if (int.TryParse(size, out int boardSize))
         {
             //2. Felhantering med while-loop ifall användaren anger värde som inte är mellan 3 och 50.
+
             while (boardSize < 3 || boardSize > 50)
             {
                 Console.Write("Ogiltig storlek. Vänligen ange ett tal mellan 3 och 50: ");
@@ -17,10 +19,12 @@
                 int.TryParse(size, out boardSize);
             }
             //3. Nestald for-loop för att skapa rad och kolumn med lika stort antal utifrån använaderns input.
-            //Översta for-loop skapar rad, och kör kolumn for-loopet inuti innan den skapar ny rad.
+            //Översta for-loop skapar rad, och kör kolumn for-loopet inuti innan den skapar ny rad. 
+
             for(int rows = 0; rows < boardSize; rows++)
             {
-                //4. Skapa ny columns, modulo operator används för att byta mellan de två tecken som finns för varje plats.
+                //4. Skapa ny columns, modulo operator används för att byta mellan de två tecken som finns för varje plats. 
+
                 for(int columns = 0; columns < boardSize; columns++)
                 {
                     if((rows + columns) % 2 == 0)
