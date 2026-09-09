c114c9f3d239a8494388790f12aab0837edf3c7b
Linus Karjalainen <linus.karja@gmail.com>
2026-09-09 09:33:17 +0200

    Validate chessboard size input

--- Chessboard/Program.cs
+++ Chessboard/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.Write("Hur stor ska brädet vara? skriv mellan 3 och 50: ");
+
+string size = Console.ReadLine();
+
+if(int.TryParse(size, out int boardSize))
+{
+    while(boardSize < 3 || boardSize > 50)
+    {
+        Console.Write("Ogiltig storlek. Vänligen ange ett tal mellan 3 och 50: ");
+        size = Console.ReadLine();
+        int.TryParse(size, out boardSize);
+    }
+
+}
