f568ddc3be553fa9941b65231e86dee11b293e58
Mohammed Alwan <mohammedalwan12345@gmail.com>
2026-09-17 12:50:00 +0200

    Revert "Finished product & updated README and comments"
    This reverts commit 4409cf4763510f0024553ade96fccd2215a9f098.

--- Chessboard/Program.cs
+++ Chessboard/Program.cs
@@ -9,26 +9,28 @@ internal class Program
         //felhanteras of TryParse() i if-sats och else-sats genom att anropa Size_Input metoden
         //if- och else-sats för att kontrollera om värdet är en integer. Kallar statisk metod från BoardRendering klassen om...
         //...integer är rätt, för att skriva ut schackbrädet. Annars startas om programmet.
-        //while-loop används för att senare låta användaren välja om de vill stänga programmet eller fortsätta.
 
         bool active = true;
         while (active)
         {
-            //Anropa metod för att be användaren om input, returnera värdet och lagra det i den lokala variablen "size".
-            
+            //Console.Write("Hur stor ska brädet vara? skriv mellan 3 och 50: ");
+
+            //string size = Console.ReadLine();
             string size = User_Answer();
 
-            //Anropa Size_Input() metoden så att "size" omvandlas till en integer utan fel.
-            //Om konvertering från "size" strängen har lyckats så lagras den i "boardSize" varriabel.
-            
             int boardSize = Size_Input(size);
 
-            //Anropa statisk metod från BoardRendering-klassen, och skicka "boardSize" integer som argument för att skriva ut schakbrädet.
-            
-            BoardRendering.Render_Chessboard(boardSize);      
+            if (boardSize > 0)
+            {
+                BoardRendering.Render_Chessboard(boardSize);
+            }
+            else
+            {
+                Main(null);
+            }
 
             //Frågar användaren ifall den vill bygga ett till schackbräde
-            Console.Write("Vill du göra ett till schackbräde? (y/n): ".Pastel(ConsoleColor.Green));
+            Console.WriteLine("Vill du göra ett till schackbräde? (y/n) ".Pastel(ConsoleColor.Green));
             string answer = Console.ReadLine().ToLower();
 
             if (answer != "y")
@@ -41,26 +43,40 @@ internal class Program
     }
             
         
-    //Metod som ska omvandla "size" sträng input till en integer. "size" skickas som argument från Main metoden.
-    //Inkluderar enkel felhantering med hjälp av en while-loop, som integrerar en kombinerad villkorsuttryck med hjälp av TryParse()
-    //och jämförelse. 
-    
+
     static int Size_Input(string size)
     {
         int boardSize;
         while (!int.TryParse(size, out boardSize) || boardSize < 3 || boardSize > 50)
         {
-            Console.Beep();
             Console.Write("Ogiltig storlek eller tecken. Vänligen ange ett tal mellan 3 och 50: ".Pastel(ConsoleColor.Red));
             size = Console.ReadLine();
         }
         return boardSize;
 
+        //        //Kontrollera om användarens input är integer
+        //        if (int.TryParse(size, out int boardSize))
+        //        {
+        //            //Felhantering med while-loop ifall användaren anger värde som inte är mellan 3 och 50.
+        //            while (boardSize < 3 || boardSize > 50)
+        //            {
+        //                Console.Write("Ogiltig storlek eller tecken. Vänligen ange ett tal mellan 3 och 50: ");
+        //                size = Console.ReadLine();
+        //                int.TryParse(size, out boardSize);
+        //            }
+        //            return boardSize;
+        //        }
+        //        //Om input är inte integer, returnera värde och starta om programmet.
+        //        else
+        //        {
+        //            Console.WriteLine("Ogiltig storlek eller tecken. Vänligen ange ett tal mellan 3 och 50.");
+        //            return boardSize;
+        //        }
+
+
 
     }
 
-    //Metod för att användaren ska mata in ett svar, i form av sträng. Sträng-värdet returneras.
-    
     static string User_Answer()
     {
         Console.Write("Hur stor ska brädet vara? skriv mellan 3 och 50: ".Pastel(ConsoleColor.Green));
